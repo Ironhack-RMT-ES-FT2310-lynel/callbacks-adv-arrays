f165e3945e0092ed8f2d1aef34114fb6edb6d3c2
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-10-16 11:02:03 +0200

    forEach done

--- script.js
+++ script.js
@@ -22,9 +22,50 @@ function funcionQueAceptaAOtraFuncion( unaFuncion, parametro ) {
 }
 
 
-
 funcionQueAceptaAOtraFuncion( decirHola, "Antonio" )
 funcionQueAceptaAOtraFuncion( decirEdad, 20 )
 
 funcionQueAceptaAOtraFuncion( decirHola, "Alejandro" )
 funcionQueAceptaAOtraFuncion( decirEdad, 19 )
+
+
+
+// forEach
+
+
+
+let randomNumbers = [ 4, 8, 15, 16, 23, 42 ];
+
+
+
+// const testFuncion = 
+
+// randomNumbers.forEach( (elNumero, index, arr) => {
+//   // no hace nada con un return
+//   // el forEach hace una ejecucion por cada elemento del array
+//   console.log("ejecutando funcion", elNumero, index, arr)
+// } )
+
+
+randomNumbers.forEach( (eachNumber) => {
+
+
+  // console.log(eachNumber * 1000)
+  if ( eachNumber === 42) {
+    console.log(eachNumber, "la respuesta a la vida, el universo y todo lo demas")
+  } else {
+    console.log(eachNumber, "cualquier numero random")
+  }
+
+} )
+
+// bucle tradicional para compara el forEach
+for (let i = 0; i < randomNumbers.length; i++) {
+
+  if ( randomNumbers[i] === 42) {
+    console.log(randomNumbers[i], "la respuesta a la vida, el universo y todo lo demas")
+  } else {
+    console.log(randomNumbers[i], "cualquier numero random")
+  }
+
+}
